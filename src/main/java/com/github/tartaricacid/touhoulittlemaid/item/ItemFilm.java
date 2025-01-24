edd15afced30dbe4cbd744074b558abcee534e53
tartaric_acid <baka943@qq.com>
2025-01-24 16:58:35 +0800

    胶片移除药水效果

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemFilm.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/item/ItemFilm.java
@@ -87,6 +87,7 @@ public class ItemFilm extends AbstractStoreMaidItem {
         nbt.remove("TicksFrozen");
         nbt.remove("HasVisualFire");
         nbt.remove("Passengers");
+        nbt.remove("ActiveEffects");
     }
 
     @Override
